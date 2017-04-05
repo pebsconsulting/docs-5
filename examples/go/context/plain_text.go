531a4e052c78c811fe91e2d1a578fd8ff07d302a
Igor Mihalik <igm@users.noreply.github.com>
2017-04-05 14:35:30 +0200

    Update plain_text.go

--- examples/go/context/plain_text.go
+++ examples/go/context/plain_text.go
@@ -8,29 +8,22 @@ import (
 )
 
 type Response []struct {
-	UID    string   `json:"uid"`
-	Weight int      `json:"weight"`
-	Stated string   `json:"stated,omitempty"`
-	Edges  []string `json:"edges,omitempty"`
+	UID    string   
+	Weight int      
+	Stated string   
+	Edges  []string 
 }
 
 func main() {
-
 	url := "http://u01.unigraph.rocks/api/context/text"
-
 	payload := strings.NewReader("Larry Page and Sergey Brin")
-
 	req, _ := http.NewRequest("POST", url, payload)
-
-	req.Header.Add("max", "20") // maximum 50
+	req.Header.Add("x-unigraph-api-max", "20") // maximum 50
 	req.Header.Add("x-unigraph-api-key", "WebDemos")
 
 	res, _ := http.DefaultClient.Do(req)
-
 	defer res.Body.Close()
 	body, _ := ioutil.ReadAll(res.Body)
-
 	fmt.Println(res)
 	fmt.Println(string(body))
-
 }
