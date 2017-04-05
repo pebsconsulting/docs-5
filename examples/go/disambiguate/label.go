c581eca4bd0240d8a0054b48f2d2e9c2562e5392
Martin Linkov <marfuzii@gmail.com>
2017-04-05 14:32:55 +0200

    Proper Key

--- examples/go/disambiguate/label.go
+++ examples/go/disambiguate/label.go
@@ -20,9 +20,7 @@ func main() {
 
 	req, _ := http.NewRequest("POST", url, payload)
 
-	req.Header.Add("x-unigraph-api-key", "1c42f9c1ca2f65441465b43cd9339d6c")
-	req.Header.Add("cache-control", "no-cache")
-	req.Header.Add("postman-token", "e6a951ed-36a6-9705-8ef2-e8a602b0fea0")
+	req.Header.Add("x-unigraph-api-key", "WebDemos")
 
 	res, _ := http.DefaultClient.Do(req)
 
